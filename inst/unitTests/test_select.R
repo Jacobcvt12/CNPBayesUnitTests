6850277abc1c13756462f24f459b779d2ab13c75
Jacob Carey <jacobcvt12@gmail.com>
2015-08-26 15:43:53 -0400

    Check that marginal is preferred over batch.

--- inst/unitTests/test_select.R
+++ inst/unitTests/test_select.R
@@ -39,3 +39,34 @@ test_compare_galaxy_batch <- function() {
     checkTrue(lik.batch[3] >= max(lik.batch))
     checkTrue(lik.batch[3] >= lik.marginal[3])
 }
+
+test_marginal_preferred <- function() {
+    library(MASS)
+    data(galaxies)
+    set.seed(123)
+
+    galaxies[78] <- 26960
+    galaxies <- c(galaxies, galaxies + 10)
+
+    mp <- McmcParams(thin=10, iter=1000, burnin=10000, nStarts=20)
+    hypp <- Hyperparameters(type="batch", k=2, m2.0=100)
+
+    model <- BatchModel(data=galaxies / 1000, k=2,
+                        hypp=hypp,
+                        batch=rep(1:2, each=length(galaxies) / 2),
+                        mcmc.params=mp)
+
+    mlist.batch <- posteriorSimulation(model)
+    lik.batch <- batchLikelihood(mlist.batch)
+
+    hypp <- Hyperparameters(type="marginal", k=2, m2.0=100)
+
+    model <- MarginalModel(data=galaxies / 1000, k=2,
+                           hypp=hypp,
+                           mcmc.params=mp)
+
+    mlist.marginal <- posteriorSimulation(model)
+    lik.marginal <- marginalLikelihood(mlist.marginal)
+
+    checkTrue(lik.batch[3] <= lik.marginal[3])
+}
