8a3c0c5a5bc13a658cd53cdd15cf6105c5e8dd20
Robert Scharpf <rscharpf@Roberts-MacBook-Pro.local>
2015-09-22 21:26:57 -0400

    Move unitTests/test_batch.R and test_loglik to testthat/

--- tests/testthat/test_loglik.R
+++ tests/testthat/test_loglik.R
@@ -1,16 +1,20 @@
-test_senseless_batch <- function(){
+test_that("senseless_batch"{
   set.seed(2000)
   library(oligoClasses)
   truth <- simulateData(N=2500,
                         theta=c(-2, -0.4, 0),
                         sds=c(0.3, 0.1, 0.1),
                         p=c(0.005, 1/10, 1-0.005-1/10))
-  m1 <- computeMarginalLik(y(truth), nchains=5,
-                           burnin=300,
-                           T2=500, T=1000)
-  models <- orderModels(m1)
+  mp <- McmcParams(iter=1000, burnin=300, nStarts=5)
+  model3 <- MarginalModel(y(truth), mcmc.params=mp, k=3)
+  model3 <- posteriorSimulation(model3)
+  model2 <- MarginalModel(y(truth), mcmc.params=mp, k=2)
+  model2 <- posteriorSimulation(model2)
+  mlist <- list(model2, model3)
+  x <- marginalLikelihood(mlist)
+  models <- mlist[order(x, decreasing=TRUE)]
   ## might select k=2 due to variability of marginal lik estimates
-  checkTrue(k(models)[1] == 3)
+  expect_true(k(models[[1]]) == 3)
   if(FALSE){
     ## Make up a bogus batch
     m2 <- computeMarginalLik(y(truth), batch=rep(1:3, length.out=2500),
